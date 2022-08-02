32c28e1a4988efbee11050c0cc05ee07faab2b51
DrCharger <dmitriy.nasa@gmail.com>
2022-08-02 18:59:12 +0300

    tasjk2

---
+++ lesson21/task2/index.js
@@ -0,0 +1,9 @@
+const getTitleElement = () => {
+	const titleElem = document.querySelector('.title');
+	return console.dir(titleElem);
+};
+
+const getInputElement = () => {
+	const inputElem = document.querySelector('[type=text]');
+	return console.dir(inputElem);
+};
