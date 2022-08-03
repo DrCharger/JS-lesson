71383bfc45d8f7e35e00ccc0b5ec5d6b3525fbdd
DrCharger <dmitriy.nasa@gmail.com>
2022-08-03 13:32:40 +0300

    2

--- lesson21/task8/index.js
+++ lesson21/task8/index.js
@@ -1,4 +1,4 @@
-const createButton = (buttonText) => {
+export const createButton = (buttonText) => {
 	let btn = document.createElement('button');
 	btn.textContent = buttonText;
 	return document.querySelector('body').append(btn);
