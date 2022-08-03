b4f3dd0830cb911f0093c83f9bde5ccc773e4fd8
DrCharger <dmitriy.nasa@gmail.com>
2022-08-03 14:13:36 +0300

    1

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -1,14 +1,14 @@
 export const finishList = () => {
-	let one = document.createElement('li');
-	one.textContent = '1';
-	document.querySelector('.list').prepend(one);
-	let eight = document.createElement('li');
+	const eight = document.createElement('li');
 	eight.textContent = '8';
 	document.querySelector('.list').append(eight);
-	let four = document.createElement('li');
+	const one = document.createElement('li');
+	one.textContent = '1';
+	document.querySelector('.list').prepend(one);
+	const four = document.createElement('li');
 	four.textContent = '4';
 	document.querySelector('.special').before(four);
-	let six = document.createElement('li');
+	const six = document.createElement('li');
 	six.textContent = '6';
 	document.querySelector('.special').after(six);
 };
