222b4c0146a3ddb63170a33ad92cc4920f56da0d
DrCharger <dmitriy.nasa@gmail.com>
2022-08-03 16:35:11 +0300

    5

--- lesson21/task12/index.js
+++ lesson21/task12/index.js
@@ -1,8 +1,10 @@
 export const squaredNumbers = () => {
 	const a = Array.from(document.querySelectorAll('.number'));
-	// .setAttribute('data-squared', null);
 	console.log(a);
 	a.map((num) =>
-		num.setAttribute('data-squared', num.dataset.number * num.dataset.number),
+		num.setAttribute(
+			'data-squared-number',
+			num.dataset.number * num.dataset.number,
+		),
 	);
 };
