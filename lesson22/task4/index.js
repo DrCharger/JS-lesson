286f7c2a31f4a8df17501c6b812eb05df92c62d4
DrCharger <dmitriy.nasa@gmail.com>
2022-08-08 17:21:36 +0300

    1

--- lesson22/task4/index.js
+++ lesson22/task4/index.js
@@ -1,9 +1,5 @@
 const checkboxElem = document.querySelector('.task-status');
 
-checkboxElem.addEventListener('change', () => {
-	if (checkboxElem.checked) {
-		console.log('true');
-	} else {
-		console.log('false');
-	}
-});
+checkboxElem.addEventListener('change', () =>
+	checkboxElem.checked ? console.log('true') : console.log('false'),
+);
