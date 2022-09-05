1d68aa3ecb7fd80bf519fdde4ae8484351311336
DrCharger <dmitriy.nasa@gmail.com>
2022-09-05 11:56:25 +0300

    sdf

---
+++ lesson30/tassk5/index.js
@@ -0,0 +1,17 @@
+import { addImage } from './addImage.js';
+
+const addImageV2 = (url) => {
+	const p = new Promise((resolve, reject) => {
+		addImage(url, (err, script) => {
+			if (err) reject(err);
+			else resolve(script);
+		});
+	});
+	return p;
+};
+
+// examples
+
+addImageV2('https://server.com/image.png')
+	.then((data) => console.log(data)) // ==> { width: 200, height: 100 }
+	.catch((error) => console.log(error)); // ==> 'Image load failed'
