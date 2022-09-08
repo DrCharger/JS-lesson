c14e52ed00d362ad31d5e663d3410e7faf8d5aa1
DrCharger <dmitriy.nasa@gmail.com>
2022-09-08 12:47:55 +0300

    todo

--- lesson33/task3/index.js
+++ lesson33/task3/index.js
@@ -1,22 +1,18 @@
-// const defaultUrl =
-// 	'https://api.github.com/repos/USERID/REPOID/commits?per_page=100';
 const date = (days) => {
 	const dateLast = JSON.parse(
 		JSON.stringify(new Date(new Date().setDate(new Date().getDate() - days))),
 	);
-	const dateNow = JSON.parse(JSON.stringify(new Date()));
-	const newUrl = `${dateLast}..${dateNow}`;
-	return newUrl;
+	return `${dateLast}`;
 };
 
-const fetchUserData = (days) => {
+const fetchRepoById = (days, userId, repoId) => {
 	return fetch(
-		`https://api.github.com/search/commits?q=committer-date:${date(
-			days,
-		)}&per_page=100&sort`,
-	).then((value) => value.json());
+		`https://api.github.com/repos/${userId}/${repoId}/commits?sinse:
+		${date(days)}&per_page=100`,
+	).then((response) => response.json());
 };
-const foo = (userData) => {
+
+const findMostActive = (userData) => {
 	const result = [];
 	let maxCount = 0;
 	const newArr = userData.map((elem) => elem.commit.author);
@@ -24,25 +20,30 @@ const foo = (userData) => {
 		.filter((el) => {
 			if (result.includes(el.email)) {
 				newArr.find((el2) => (el2.email === el.email ? el2.count++ : null));
+			} else {
+				el.count = 1;
+				delete el.date;
+				result.push(el.email);
+				return el;
 			}
-			el.count = 1;
-			delete el.date;
-			result.push(el.email);
-			return el;
 		})
 		.map((el) => {
 			if (el.count >= maxCount) {
 				maxCount = el.count;
 			}
 			return el;
-		})
-		.filter((el) => el.count === maxCount);
+		});
+	// .filter((el) => el.count === maxCount);
 };
 
 const getMostActiveDevs = ({ days, userId, repoId }) => {
-	fetchUserData(days)
-		.then((finded) => foo(finded.items))
-		.then((result) => console.log(result));
+	return fetchRepoById(days, userId, repoId)
+		.then((finded) => findMostActive(finded))
+		.then((r) => console.log(r));
 };
 
-getMostActiveDevs({ days: 5, userId: 'user-16', repoId: 'repo-1' });
+getMostActiveDevs({
+	days: 7,
+	userId: 'ratschlab',
+	repoId: 'metagraph',
+});
